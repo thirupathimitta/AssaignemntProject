4d3ac60bef2e9faae4a74ff6cd356f40cd3c16df
thirupathimitta <mittathirupathi7@gmail.com>
2023-09-15 07:30:40 +0530

    assaigment project

---
+++ SpringProjectAssaignment/src/main/java/com/asssigment/SpringProjectAssaignmentApplication.java
@@ -0,0 +1,13 @@
+package com.asssigment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectAssaignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectAssaignmentApplication.class, args);
+	}
+
+}
---
+++ SpringProjectAssaignment/src/test/java/com/asssigment/SpringProjectAssaignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asssigment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectAssaignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
